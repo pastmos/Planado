7d6320299879d08490d116ef25675fe069a82981
pastmos <passt@yandex.ru>
2017-10-06 22:13:13 +0300

    Back button in OrdersListViewController added

--- Planado/OrdersListTableViewController.swift
+++ Planado/OrdersListTableViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class OrdersListTableViewController: UITableViewController {
 
+    @IBAction func backButton(_ sender: Any) {
+        //First method is to dissmis current controller and go back to previous automatically
+        //in completion closure you can do any actions you want before dismissing current controller
+        self.dismiss(animated: true, completion: nil)
+        
+//        //Second method is to jump to any specified controller
+//        //and you must set Storyboard ID in Identity Inspector as LoginViewControllerID for LoginViewController before to avoid black screen and to define identifier for instantiateViewController method
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//        let vc = storyboard.instantiateViewController(withIdentifier: "LoginViewControllerID")
+//        self.present(vc, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,11 +33,11 @@ class OrdersListTableViewController: UITableViewController {
     }
     
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return orders.count
@@ -36,13 +47,13 @@ class OrdersListTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "OrderCellIdentifier", for: indexPath) as! OrderCell
         
-       let order = orders[indexPath.row] as Order
+        let order = orders[indexPath.row] as Order
         cell.startTime?.text = order.startTime
         cell.workType?.text = order.workType
         cell.customerName?.text = order.customerName
         cell.customerAddress?.text = order.customerAddress
-
-      return cell
+        
+        return cell
     }
   
 
