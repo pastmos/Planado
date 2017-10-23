f4dde5a3a16a42778cc8e37931a50d79bab7b5ed
pastmos <passt@yandex.ru>
2017-10-23 19:04:50 +0300

    FireBase initialized and connected to the project through CocoaPods

--- Planado/Supporting Files/AppDelegate.swift
+++ Planado/Supporting Files/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 
 @UIApplicationMain
@@ -21,6 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
---
+++ Pods/Target Support Files/Pods-Planado/Pods-Planado-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Planado : NSObject
+@end
+@implementation PodsDummy_Pods_Planado
+@end
